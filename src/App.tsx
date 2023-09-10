389e7a925299503c98f0b384bce0ee9ee86e61e1
jakecastor09 <rencelaw70@gmail.com>
2023-09-10 15:56:23 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <h1 className='container mx-auto text-2xl max-w-xs text-center  font-bold text-darkViolet-1000 md:max-w-md md:text-3xl lg:text-6xl lg:md:max-w-2xl'>
+      Access expert medical advice through our app
+    </h1>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        darkYellow: '#ffb309',
+        lightBlue: '#367ce8',
+        lightGreen: '#2DAA66',
+        lightCyan: '#41FFDD',
+        lightOrange: '#FC9C56',
+
+        darkViolet: {
+          1000: '#240046',
+          900: '#251C30',
+          800: '#31293B',
+          700: '#3D3547',
+          600: '#4A4153',
+          500: '#584E61',
+          400: '#675C6F',
+          300: '#776B7F',
+          200: '#998DA1',
+          100: '#C3B8CC',
+        },
+      },
+    },
+    fontFamily: {
+      sans: ['Manrope', 'san-serif'],
+    },
+  },
+  plugins: [],
+};
