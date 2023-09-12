ddb0863e68374fdd23ad19fc029f3c41e210c62a
jakecastor09 <rencelaw70@gmail.com>
2023-09-12 21:19:10 +0800

    Update: adding hover in consult item

--- src/components/Consult/ConsultItem.tsx
+++ src/components/Consult/ConsultItem.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 const ConsultItem = ({ imgSrc, name }: Props) => {
   return (
-    <div className='rounded-3xl p-8 bg-white flex space-y-4 flex-col items-center'>
+    <div className='shadow-sm rounded-3xl p-8 bg-white flex space-y-4 flex-col items-center hover:outline-darkViolet-1000 hover:outline duration-100'>
       <div className='w-16 h-16 p-4  rounded-full bg-whiteMagnolia flex justify-center'>
         <img src={imgSrc} alt='' />
       </div>
