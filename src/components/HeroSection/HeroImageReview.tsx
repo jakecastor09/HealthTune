0cebd74e12b54a7cbdf88869b1da51335d4e8c5e
jakecastor09 <rencelaw70@gmail.com>
2023-09-12 11:10:20 +0800

    Debug: image can't view in production

--- src/components/HeroSection/HeroImageReview.tsx
+++ src/components/HeroSection/HeroImageReview.tsx
@@ -1,11 +1,20 @@
+import review1 from '../../assets/images/review-1.webp';
+import review2 from '../../assets/images/review-2.webp';
+import review3 from '../../assets/images/review-3.webp';
+
 interface Props {
   img: 1 | 2 | 3;
   styles?: string;
 }
 const HeroImageReview = ({ img, styles }: Props) => {
+  const images: { [key in 1 | 2 | 3]: string } = {
+    1: review1,
+    2: review2,
+    3: review3,
+  };
   return (
     <img
-      src={`../../images/review-${img}.webp`}
+      src={images[img]}
       alt=''
       className={`${styles} rounded-full w-14 h-14 object-cover border-4 border-white md:w-12 md:h-12 md:border-3 `}
     />
