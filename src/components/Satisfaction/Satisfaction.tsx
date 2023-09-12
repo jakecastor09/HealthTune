e983fee339c7d474cda638da129c75426326c32b
jakecastor09 <rencelaw70@gmail.com>
2023-09-12 10:48:04 +0800

    Refactor: Satisfaction

--- src/components/Satisfaction/Satisfaction.tsx
+++ src/components/Satisfaction/Satisfaction.tsx
@@ -1,16 +1,19 @@
 import Container from '../Container/Container';
 import SatisfactionContainer from './SatisfactionContainer';
 import SatisfactionItem from './SatisfactionItem';
-import SatisfactionItemWithBorder from './SatisfactionItemWithBorder';
 
 const Satisfaction = () => {
   return (
     <Container styles='bg-white'>
       <SatisfactionContainer>
-        <SatisfactionItemWithBorder />
-        <SatisfactionItemWithBorder />
-        <SatisfactionItem />
-        <SatisfactionItem />
+        <SatisfactionItem number={50} text='Country Coverage' border={true} />
+        <SatisfactionItem
+          number={800}
+          text='Clieny Satisfaction'
+          border={true}
+        />
+        <SatisfactionItem number={100} text='Expert Consultant' />
+        <SatisfactionItem number={250} text='Clieny Satisfaction' />
       </SatisfactionContainer>
     </Container>
   );
--- src/components/Satisfaction/SatisfactionItem.tsx
+++ src/components/Satisfaction/SatisfactionItem.tsx
@@ -1,8 +1,18 @@
-const SatisfactionItem = () => {
+interface Props {
+  text: string;
+  number: number;
+  border?: boolean;
+}
+const SatisfactionItem = ({ text, number, border }: Props) => {
   return (
     <div className='space-y-4 md:space-y-0 md:w-full'>
-      <h1 className='text-darkViolet-1000 text-2xl font-extrabold'>50+</h1>
-      <p className='text-darkViolet-600'>Country Coverage</p>
+      <h1 className='text-darkViolet-1000 text-2xl font-extrabold'>
+        {number}+
+      </h1>
+      <p className='text-darkViolet-600'>{text}</p>
+      {border && (
+        <div className='border border-dashed border-gray-200 w-1/2 md:hidden'></div>
+      )}
     </div>
   );
 };
